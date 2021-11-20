a130a8f18f1b29eac7abc0de333bcfae1add9697
furkanturunc <furkan.turunc@ug.bilkent.edu.tr>
2021-11-20 21:01:38 +0300

    Sign In/Up and Edit_Profile Pages added

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,56 @@
+import { makeStyles } from "@material-ui/core/styles";
+import userLogo from "../images/userLogo.png";
+import {Button, TextField, Link} from "@material-ui/core";
+
+
+const useStyles = makeStyles({
+    loginContainer: {
+        width: "15%",
+        height: "40%",
+        marginLeft: "42.5%",
+        marginTop:"10%",
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+
+    title: {
+        color: "#023047",
+        fontSize:"20px",
+    },
+
+    textfield: {
+        width:"100%",
+        height: "20px",
+        borderRadius: "20px",
+        marginTop:"20%",
+    },
+
+    loginButton: {
+        marginTop: "20%",
+        width: "100%",
+        background:"#023047",
+        color:"#ffffff",
+        marginBottom:"8%",
+    }
+
+});
+
+const Login = ()=>{
+    const classes = useStyles();
+
+    return (
+        <div className={classes.loginContainer}>
+            <p className={classes.title}>Welcome to Visual Pandemic</p>
+            <img src={userLogo} alt=""/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" className={classes.textfield}/>
+            <TextField id="outlined-basic" label="Password" variant="outlined" className={classes.textfield}/>
+            <Button variant="contained" className={classes.loginButton}>Login</Button>
+            <Link href="#">Forgot Password?</Link>
+            <p>Don't have an account? <Link href="#">Register</Link></p>
+        </div>
+
+    );
+}
+export default Login;
