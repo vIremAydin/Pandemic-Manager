0bca6be02397754f5be4e60babbcca4501c93269
ardabdotortlek <arda_ege33@hotmail.com>
2021-12-15 15:25:51 +0300

    Back-End File has been added.

---
+++ back-end/src/test/java/com/group1j/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1j.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
