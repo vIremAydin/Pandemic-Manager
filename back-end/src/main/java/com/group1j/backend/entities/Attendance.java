5188f538c18d3ab73d70b31c80af1ff1ff337e39
ardabdotortlek <arda_ege33@hotmail.com>
2021-12-16 23:02:31 +0300

    Attendance class has been modified.

--- back-end/src/main/java/com/group1j/backend/entities/Attendance.java
+++ back-end/src/main/java/com/group1j/backend/entities/Attendance.java
@@ -1,4 +1,21 @@
 package com.group1j.backend.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Attendance {
+    //Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private int attendanceID;
+
+    @ElementCollection
+    @CollectionTable(name = "active_students_list", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "student_id")
+    private List<Integer> activeStudents;
+
+    private String date;
+    private String attendanceCode;
 }
