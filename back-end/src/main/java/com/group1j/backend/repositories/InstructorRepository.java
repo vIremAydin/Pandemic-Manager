859a25875d7ff537ff89cd79ffd73f9bc8cae617
cekole <63546159+cekole@users.noreply.github.com>
2021-12-22 23:57:49 +0300

    Added repository class for Instructor

---
+++ back-end/src/main/java/com/group1j/backend/repositories/InstructorRepository.java
@@ -0,0 +1,14 @@
+package com.group1j.backend.repositories;
+
+import com.group1j.backend.entities.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InstructorRepository extends JpaRepository<InstructorRepository,Integer> {
+
+    //@Query("SELECT x FROM Instructor x WHERE x.id = ?1")
+    Optional<Instructor> findById(Integer id);
+}
