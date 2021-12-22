5a0746b9c4b202579f3c0736a255b7aa25166194
Bilgehan Sandıkcı <59236563+Bilgehansandikci@users.noreply.github.com>
2021-12-22 20:57:54 +0300

    Some Services added,

--- back-end/src/main/java/com/group1j/backend/services/AppointmentService.java
+++ back-end/src/main/java/com/group1j/backend/services/AppointmentService.java
@@ -1,4 +1,44 @@
 package com.group1j.backend.services;
 
+import com.group1j.backend.dto.CreateUserDTO;
+import com.group1j.backend.dto.UserLoginDTO;
+import com.group1j.backend.entities.*;
+import com.group1j.backend.repositories.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AppointmentService {
+
+    private AppointmentRepository appointmentRepository;
+
+    //Constructor
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    /**
+     * Method for returning all of the students in the system
+     * @return
+     */
+    public List<Appointment> getAllAppointments(){
+        return appointmentRepository.findAll();
+    }
+
+    //public Appointment createAppointment(CreateUserDTO createUserDTO){} //TODO
+
+    public Optional<Appointment> findByAppointmentid(Integer id){
+        return appointmentRepository.findByAppointmentId(id);
+    }
+
+    public AppointmentRepository getAppointmentRepository() {
+        return appointmentRepository;
+    }
+
+    public void setAppointmentRepository(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
 }
--- back-end/src/main/java/com/group1j/backend/services/DoctorAppointmentService.java
+++ back-end/src/main/java/com/group1j/backend/services/DoctorAppointmentService.java
@@ -1,4 +1,15 @@
 package com.group1j.backend.services;
 
+import com.group1j.backend.dto.CreateUserDTO;
+import com.group1j.backend.dto.UserLoginDTO;
+import com.group1j.backend.entities.*;
+import com.group1j.backend.repositories.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DoctorAppointmentService {
 }
