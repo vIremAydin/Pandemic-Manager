7e9c78c67b77903417045036a6440dc00b773632
irem <viremaydin@gmail.com>
2021-12-25 16:16:19 +0300

    Login.js

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -56,6 +56,7 @@ const Login = ({saveUser}) => {
 
         axios.get("http://localhost:8080/api/student/login/" + id + "/" + password).then((response) => {
             console.log(response.data);
+            saveUser(response.data);
             setSuccessful(response.data);
         }).catch((error) => {
             console.log(error);
